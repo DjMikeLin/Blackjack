b99df912c6a7568b5bb31b757efe3c413ce5cea5
MikeLin <linmichael97@yahoo.com>
2019-04-10 11:38:14 -0400

    1. Added Deck images 2. Added jquery 3. Working on Deck class

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+class Deck{
+    constructor(){
+        this.numCards = 52; 
+    }
+
+    makeDeck(){
+        let arr = [];
+
+        for(let i = 0; i < 52; i++){
+            
+        }
+    }
+}
