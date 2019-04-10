52a245fbae965e94e0aca0995d4801407fd3a455
MikeLin <linmichael97@yahoo.com>
2019-04-10 18:18:28 -0400

    Implemented infinite loop AI for dealer

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,13 @@ $(() => {
     let game = new Game();
     game.player.assignCards(game.currRound.deck.deal(2));
     game.dealer.assignCards(game.currRound.deck.deal(2));
-    game.player.stand = true;
-    console.log(game.player);
+    
+    while(!game.dealer.stand){
+        if(game.dealer.score <= 16)
+            game.dealer.assignCards(game.currRound.deck.deal(1));
+        else
+            game.dealer.stand = true;
+    }
+
+    console.log(game);
 });
