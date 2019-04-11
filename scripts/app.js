4734ef9fd61ef7716d949ca13faa09d49c72ff50
MikeLin <linmichael97@yahoo.com>
2019-04-11 12:49:44 -0400

    Make cards appear top middle

--- scripts/app.js
+++ scripts/app.js
@@ -3,16 +3,7 @@ $(() => {
     let dealer = new Player(0);
     let game = new Game(player, dealer);
     player.assignCards(game.currRound.deck.deal(2));
-
-    let right = 0;
-    for(let i = 0; i < player.cards.length; i++){
-        let item = $('<img></img>').addClass('cards')
-            .attr("src", player.cards[i].path)
-            .css('right', right + 'px');
-        $('.playerCards').append(item);
-
-        right += 50;
-    }
+    showCards(player, $('.playerCards'));
     console.log(player);
     dealer.assignCards(game.currRound.deck.deal(2));
     console.log(dealer);
@@ -29,15 +20,15 @@ $(() => {
 /*     console.log(game);
     console.log(game.dealer.cards); */
 });
-
-function showCards(player){
+//Sends the card images that the player has to dom
+function showCards(player, selector){
     let right = 0;
     for(let i = 0; i < player.cards.length; i++){
         let item = $('<img></img>').addClass('cards')
             .attr("src", player.cards[i].path)
             .css('right', right + 'px');
-        $('.playerCards').append(item);
 
+        selector.append(item);
         right += 50;
     }
 }
